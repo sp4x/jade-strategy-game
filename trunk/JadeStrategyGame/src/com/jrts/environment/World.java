9d234d3bbc853211a3e6239489e3d6f3e6ac85c0
linuspax <linuspax@7573b069-5e2a-cb2a-817b-d34f5295028c>
2012-04-15 12:05:40 +0000

    metodo getPerception in World

--- trunk/JadeStrategyGame/src/com/jrts/environment/World.java
+++ trunk/JadeStrategyGame/src/com/jrts/environment/World.java
@@ -78,6 +78,11 @@ public class World {
 		return floor.get(p.row, p.col) == Cell.FREE;
 	}
 	
+	
+	/**
+	 * adds the city centre in a random position for a new team with the specified name
+	 * @param name the name of the team, has to be unique
+	 */
 	public synchronized void addTeam(String name) {
 		Random r = new Random();
 		Cell base = Cell.BUILDING;
@@ -89,26 +94,57 @@ public class World {
 		teams.put(name, p);
 	}
 	
-	public synchronized boolean addUnit(String id, Position p) {
+	
+	/**
+	 * adds a unit in the specified position or in the neighborhood if it is not free
+	 * @param id an identification string for the unit, should be unique for the world
+	 * @param p the position to use. Can be the position of a factory
+	 * @return the position where the unit has been created
+	 */
+	public synchronized Position addUnit(String id, Position p) {
 		Cell unit = Cell.UNIT;
 		unit.id = id;
 		if (!isAvailable(p)) {
 			p = nextTo(p);
 			if (p == null) {
-				System.out.println("cannot add unit");
-				return false;
+				return null;
 			}
 		}
 		floor.set(p.row, p.col, unit);
-		return true;
+		return p;
 	}
 	
+	/**
+	 * gets the position of the city centre of the team with the specified name
+	 * @param teamName the name of the team
+	 * @return the position of the main building
+	 */
 	public synchronized Position getBuilding(String teamName) {
 		return teams.get(teamName);
 	}
 
 	public Floor getFloor() {
 		return floor;
+	}
+	
+	
+	/**
+	 * Returns the perceived floor in a certain position with the specified range of sight
+	 * @param centre where the observer is located
+	 * @param sight the range of sight of the observer
+	 * @return a floor object where the perceived cells are the same of the world's floor
+	 * and the others are set to UNKNOWN
+	 */
+	public synchronized Floor getPerception(Position centre, int sight) {
+		Floor perception = new Floor(floor.rows, floor.cols);
+		for (int row = 0; row < floor.rows; row++) {
+			for (int col = 0; col < floor.cols; col++) {
+				int sumOfDiff = Math.abs(centre.row-row) + Math.abs(centre.col-col);
+				Cell perceived = (sumOfDiff <= sight ? floor.get(row, col) : Cell.UNKNOWN);
+				perception.set(row, col, perceived);
+			}
+		}
+		return perception;
 	}
 
 }
