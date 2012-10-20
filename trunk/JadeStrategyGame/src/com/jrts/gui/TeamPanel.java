676d506329a17bf9b75dc0bb61529e71fd066737
neb.inf <neb.inf@7573b069-5e2a-cb2a-817b-d34f5295028c>
2012-10-20 18:20:43 +0000

    Teams have now nice different colors

--- trunk/JadeStrategyGame/src/com/jrts/gui/TeamPanel.java
+++ trunk/JadeStrategyGame/src/com/jrts/gui/TeamPanel.java
@@ -1,11 +1,13 @@
 package com.jrts.gui;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
 
 import com.jrts.O2Ainterfaces.Team;
 
@@ -21,7 +23,14 @@ public class TeamPanel extends JPanel{
 		
 		this.team = team;
 		
-		super.setBorder(BorderFactory.createTitledBorder(team.getTeamName()));
+		TitledBorder border =BorderFactory.createTitledBorder(team.getTeamName());
+		
+		if(team.getTeamName().equals("team1")) border.setTitleColor(Color.RED);
+		if(team.getTeamName().equals("team2")) border.setTitleColor(Color.BLACK);
+		if(team.getTeamName().equals("team3")) border.setTitleColor(Color.YELLOW);
+		if(team.getTeamName().equals("team4")) border.setTitleColor(Color.BLUE);
+		
+		super.setBorder(border);
 		
 		super.setPreferredSize(new Dimension(150, 50));
 
