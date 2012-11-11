edbc86ce2959ce9d8e560a5fbc8b624215a3ec83
linuspax <linuspax@7573b069-5e2a-cb2a-817b-d34f5295028c>
2012-11-11 15:27:04 +0000

    optimized nextTo

---
+++ trunk/JadeStrategyGame/test/com/jrts/environment/FloorTest.java
@@ -0,0 +1,26 @@
+package com.jrts.environment;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FloorTest {
+
+	@Test
+	//+---
+	//-*--
+	//--x-
+	//----
+	public void test1() {
+		Floor f = new Floor(4, 4);
+		Position p = f.nextTo(new Position(0, 0), new Position(2, 2), CellType.FREE, 3);
+		assertEquals(new Position(1, 1), p);
+	}
+	
+	public void test2() {
+		Floor f = new Floor(40, 40);
+		Position p = f.nextTo(new Position(0, 0), new Position(38, 38), CellType.FREE, 5);
+		assertEquals(new Position(37, 37), p);
+	}
+
+}
