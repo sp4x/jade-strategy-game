5cb69868bdfe0daf8a0a79f82d703dec6fb867ef
andreamartire <andreamartire@7573b069-5e2a-cb2a-817b-d34f5295028c>
2012-11-16 20:06:00 +0000

    New Explore Behaviour

--- JadeStrategyGame/src/com/jrts/behaviours/BehaviourWrapper.java
+++ JadeStrategyGame/src/com/jrts/behaviours/BehaviourWrapper.java
@@ -5,9 +5,6 @@ import jade.core.behaviours.CyclicBehaviour;
 
 public class BehaviourWrapper extends CyclicBehaviour {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	UnitBehaviour highPriority;
